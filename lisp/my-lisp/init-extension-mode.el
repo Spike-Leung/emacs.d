be649f9c9107fbb23ad93ba4325d8306f71edb64
Spike <l-yanlei@hotmail.com>
2021-09-21 12:49:38 +0800

    refactor: rename init-vue to init-extension-mode

---
+++ lisp/my-lisp/init-extension-mode.el
@@ -0,0 +1,10 @@
+;;; Package --- Summary
+;; init-extension-mode
+;;; Commentary:
+;;; Code:
+(add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+
+(provide 'init-extension-mode)
+
+;;; init-extension-mode.el ends here
--- lisp/my-lisp/init-vue.el
+++
@@ -1,8 +0,0 @@
-;;; Package --- Summary
-;; init-vue
-;;; Commentary:
-;;; Code:
-(add-to-list 'auto-mode-alist '("\\.vue\\'" . mhtml-mode))
-
-(provide 'init-vue)
-;;; init-vue.el ends here
