11921aa30d5ad394fc01f4b61348e318d4b218d5
Spike Leung <l-yanlei@hotmail.com>
2021-11-04 03:39:36 +0800

    refactor: remove no used lisp

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -8,17 +8,15 @@
 (add-to-list 'load-path (expand-file-name "lisp/my-lisp" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "lisp/my-lisp/beancount-mode" user-emacs-directory))
 
-;; (require 'init-my-themes)
 (require 'init-modus-theme)
-(require 'init-extension-mode)
 (require 'init-mu4e)
+(require 'init-my-org)
 (require 'init-org-roam)
 (require 'init-org-download)
-(require 'init-deft)
-(require 'init-easy-hugo)
 (require 'init-my-markdown)
 (require 'beancount)
-(require 'init-my-org)
+(require 'init-easy-hugo)
+(require 'init-extension-mode)
 
 (provide 'init-local)
 ;;; init-local.el ends here
