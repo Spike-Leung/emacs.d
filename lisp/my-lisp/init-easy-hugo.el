e685af9b725aeb837833e4c84c3b51431a2ec5a8
Spike Leung <l-yanlei@hotmail.com>
2021-11-03 14:22:30 +0800

    refactor: use `maybe-require-package` to install package

--- lisp/my-lisp/init-easy-hugo.el
+++ lisp/my-lisp/init-easy-hugo.el
@@ -3,16 +3,15 @@
 
 ;;; Commentary:
 ;;; Code:
-(require 'easy-hugo)
-
-(setq
- easy-hugo-basedir "~/Documents/private-git/taxodium"
- easy-hugo-default-ext ".org"
- easy-hugo-url "https://spike-leung.github.io/taxodium/"
- easy-hugo-preview-url "http://localhost:1313/taxodium"
- )
-
-(global-set-key (kbd "C-c b") 'easy-hugo)
+(when (maybe-require-package 'easy-hugo)
+  (with-eval-after-load 'easy-hugo
+    (setq
+     easy-hugo-basedir "~/Documents/private-git/taxodium"
+     easy-hugo-default-ext ".org"
+     easy-hugo-url "https://spike-leung.github.io/taxodium/"
+     easy-hugo-preview-url "http://localhost:1313/taxodium")
+    (global-set-key (kbd "C-c b") 'easy-hugo)
+    ))
 
 (provide 'init-easy-hugo)
 ;;; init-easy-hugo.el ends here
