bb25a4dc6607a3109cbd72bdd1ac33b06c48a097
Spike Leung <l-yanlei@hotmail.com>
2021-11-05 17:09:27 +0800

    feat: turn on `auto-fill-mode` in org-roam

--- lisp/my-lisp/init-org-roam.el
+++ lisp/my-lisp/init-org-roam.el
@@ -12,19 +12,19 @@
 
 (when
     (maybe-require-package 'org-roam)
-  (with-eval-after-load 'org-roam
-
-    (org-roam-db-autosync-mode)
-
-    (global-set-key (kbd "C-c n  l") 'org-roam-buffer-toggle)
-    (global-set-key (kbd "C-c n  f") 'org-roam-node-find)
-    (global-set-key (kbd "C-c n  g") 'org-roam-graph)
-    (global-set-key (kbd "C-c n  i") 'org-roam-node-insert)
-    (global-set-key (kbd "C-c n  c") 'org-roam-capture)
-    (global-set-key (kbd "C-c n  j") 'org-roam-dailies-capture-today)
-    ))
-
-(require 'org-roam-protocol)
+  (eval-after-load 'org-roam
+    (progn
+      (org-roam-db-autosync-mode)
+      (global-set-key (kbd "C-c n  l") 'org-roam-buffer-toggle)
+      (global-set-key (kbd "C-c n  f") 'org-roam-node-find)
+      (global-set-key (kbd "C-c n  g") 'org-roam-graph)
+      (global-set-key (kbd "C-c n  i") 'org-roam-node-insert)
+      (global-set-key (kbd "C-c n  c") 'org-roam-capture)
+      (global-set-key (kbd "C-c n  j")
+                      'org-roam-dailies-capture-today)
+      (add-hook 'org-roam-find-file-hook 'turn-on-auto-fill)
+      )))
+;; (maybe-require-package 'org-roam-protocol)
 
 (provide 'init-org-roam)
 ;;; init-org-roam.el ends here
