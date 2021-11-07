a4b6c0fa7ed20823f944f6cfc6c0cae611330a56
Spike Leung <l-yanlei@hotmail.com>
2021-11-07 20:20:02 +0800

    fix: use load-library to load org-roam-ui
    Avoid open org-roam-ui when start emacs

--- lisp/my-lisp/init-org-roam-ui.el
+++ lisp/my-lisp/init-org-roam-ui.el
@@ -8,11 +8,12 @@
 
 (when (maybe-require-package 'websocket)
   (when (maybe-require-package 'simple-httpd)
-    (require 'org-roam-ui)
-    (setq org-roam-ui-sync-theme t
-          org-roam-ui-follow t
-          org-roam-ui-update-on-save t
-          org-roam-ui-open-on-start t)))
+    (load-library "org-roam-ui")))
+
+(setq org-roam-ui-open-on-start nil
+      org-roam-ui-sync-theme t
+      org-roam-ui-follow t
+      org-roam-ui-update-on-save t)
 
 (provide 'init-org-roam-ui)
 ;;; init-org-roam-ui.el ends here
