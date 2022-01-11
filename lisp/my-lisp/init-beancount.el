18c995e1f48643ffdf94d4c8b9599ce6e6ad5a20
Spike Leung <l-yanlei@hotmail.com>
2022-01-11 13:55:04 +0800

    feat: enable outline-minor-mode in beancount-mode
    Add `beancount-accounts-files` for beancount to complete accounts

--- lisp/my-lisp/init-beancount.el
+++ lisp/my-lisp/init-beancount.el
@@ -5,9 +5,14 @@
 
 (push (expand-file-name "lisp/my-lisp/beancount-mode" user-emacs-directory) load-path)
 
+(setq beancount-accounts-files
+      (directory-files "~/Dropbox/beancount/accounts/" 'full (rx ".beancount" eos)))
 (require 'beancount)
 
-(push '("\\.beancount\\'" . beancount-mode) auto-mode-alist)
+(push '("\\.\\(beancount\\|bean\\)\\'" . beancount-mode) auto-mode-alist)
 
+(add-hook 'beancount-mode-hook #'outline-minor-mode)
+(define-key beancount-mode-map (kbd "C-c C-n") #'outline-next-visible-heading)
+(define-key beancount-mode-map (kbd "C-c C-p") #'outline-previous-visible-heading)
 (provide 'init-beancount)
 ;;; init-beancount.el ends here
