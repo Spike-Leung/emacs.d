d1c9b38fb0c14f9d1df8b029d6e95089eabfd1be
Spike <l-yanlei@hotmail.com>
2022-05-04 18:20:42 +0800

    feat: require impatient-mode

--- lisp/my-lisp/init-my-markdown.el
+++ lisp/my-lisp/init-my-markdown.el
@@ -6,11 +6,16 @@
 ;;; TODO: 写一个函数，完成启动服务器，启动impatient-mode, 设置filter的步骤
 ;;; Code:
 
+(maybe-require-package 'impatient-mode)
+
 (defun my-markdown-html (buffer)
   (princ (with-current-buffer buffer
            (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script></html>"
                    (buffer-substring-no-properties (point-min) (point-max))))
          (current-buffer)))
+
+(imp-set-user-filter 'my-markdown-html)
+
 
 (provide 'init-my-markdown)
 
