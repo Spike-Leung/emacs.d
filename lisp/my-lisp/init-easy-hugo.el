1107d6c536f9aef43e95828a3516370210a61cdf
Spike <l-yanlei@hotmail.com>
2022-08-09 17:03:43 +0800

    feat: use dark theme default

--- lisp/my-lisp/init-easy-hugo.el
+++ lisp/my-lisp/init-easy-hugo.el
@@ -6,7 +6,7 @@
 (when (maybe-require-package 'easy-hugo)
   (with-eval-after-load 'easy-hugo
     (setq
-     easy-hugo-basedir "~/Documents/private-git/taxodium"
+     easy-hugo-basedir "~/Documents/git/taxodium"
      easy-hugo-default-ext ".org"
      easy-hugo-url "https://spike-leung.github.io/taxodium/"
      easy-hugo-preview-url "http://localhost:1313/taxodium")
