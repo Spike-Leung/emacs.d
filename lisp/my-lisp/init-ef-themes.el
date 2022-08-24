aacdac579a13a9f963455e68e4ec7af7fb9ef379
Spike <l-yanlei@hotmail.com>
2022-08-24 08:04:14 +0800

    feat: update ef-themes config

--- lisp/my-lisp/init-ef-themes.el
+++ lisp/my-lisp/init-ef-themes.el
@@ -8,22 +8,15 @@
 ;; Disable all other themes to avoid awkward blending:
 (mapc #'disable-theme custom-enabled-themes)
 
-;; Load the theme of choice:
-;; The themes we provide:
-;;
-;; Light: `ef-day', `ef-light', `ef-spring', `ef-summer'.
-;; Dark:  `ef-autumn', `ef-dark', `ef-night', `ef-winter'.
-(load-theme 'ef-spring :no-confirm)
-(load-theme 'ef-summer :no-confirm)
-(load-theme 'ef-winter :no-confirm)
-(load-theme 'ef-autumn :no-confirm)
-
-;; We also provide these commands, but do not assign them to any key:
-;;
-;; - `ef-themes-select'
-;; - `ef-themes-load-random'
-;; - `ef-themes-preview-colors'
-;; - `ef-themes-preview-colors-current'
+;; Load the themes
+(load-theme 'ef-spring t t)
+(load-theme 'ef-summer t t)
+(load-theme 'ef-winter t t)
+(load-theme 'ef-autumn t t)
+(load-theme 'ef-day t t)
+(load-theme 'ef-night t t)
+(load-theme 'ef-day t t)
+(load-theme 'ef-night t t)
 
 (provide 'init-ef-themes)
 ;;; init-ef-themes.el ends here
