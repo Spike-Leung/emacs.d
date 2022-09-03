5a359b51318169fc04b30c34000215dfc23fe753
Spike <l-yanlei@hotmail.com>
2022-09-03 23:28:49 +0800

    feat: update default font face

--- lisp/my-lisp/init-face.el
+++ lisp/my-lisp/init-face.el
@@ -2,7 +2,7 @@
 ;; init-face
 ;;; Commentary:
 ;;; Code:
-(set-face-attribute 'default nil :height 150 :family "Hack Nerd Font" :width "regualr")
+(set-face-attribute 'default nil :height 150 :family "Hack Nerd Font")
 
 (provide 'init-face)
 ;;; init-face.el ends here
