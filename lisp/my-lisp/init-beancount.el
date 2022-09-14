63ee53a904d3212790f0e626e19f24f8d73f8d3b
Spike <l-yanlei@hotmail.com>
2022-09-14 13:26:09 +0800

    feat: update beancount-mode config, use origin repo

--- lisp/my-lisp/init-beancount.el
+++ lisp/my-lisp/init-beancount.el
@@ -4,15 +4,34 @@
 ;;; Code:
 
 (push (expand-file-name "lisp/my-lisp/beancount-mode" user-emacs-directory) load-path)
-
-(setq beancount-accounts-files
-      (directory-files "~/Dropbox/beancount/accounts/" 'full (rx ".beancount" eos)))
 (require 'beancount)
 
 (push '("\\.\\(beancount\\|bean\\)\\'" . beancount-mode) auto-mode-alist)
-
 (add-hook 'beancount-mode-hook #'outline-minor-mode)
 (define-key beancount-mode-map (kbd "C-c C-n") #'outline-next-visible-heading)
 (define-key beancount-mode-map (kbd "C-c C-p") #'outline-previous-visible-heading)
+
+;;; https://whatacold.io/blog/2022-09-10-emacs-beancount-accout-files/
+(defvar beancount-accounts-files nil "List of account files")
+(setq beancount-accounts-files
+      (directory-files "~/Dropbox/beancount/accounts/" 'full (rx ".beancount" eos)))
+(defun w/beancount--collect-accounts-from-files (oldfun regex n)
+  (let ((keys (funcall oldfun regex n))
+        (hash (make-hash-table :test 'equal)))
+    (dolist (key keys)
+      (puthash key nil hash))
+    ;; collect accounts from files
+    (save-excursion
+      (dolist (f beancount-accounts-files)
+        (with-current-buffer (find-file-noselect f)
+          (goto-char (point-min))
+          (while (re-search-forward beancount-account-regexp nil t)
+            (puthash (match-string-no-properties n) nil hash)))))
+    (hash-table-keys hash)))
+
+(advice-add #'beancount-collect
+            :around #'w/beancount--collect-accounts-from-files
+            '((name . "collect accounts from files as well")))
+
 (provide 'init-beancount)
 ;;; init-beancount.el ends here
