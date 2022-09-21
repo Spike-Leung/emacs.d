5500a1c26de5f940a90b846b744bb712ce70296a
Spike <l-yanlei@hotmail.com>
2022-09-21 14:24:41 +0800

    refactor: update impatient-mode markdown setting

--- lisp/my-lisp/init-my-markdown.el
+++ lisp/my-lisp/init-my-markdown.el
@@ -3,19 +3,27 @@
 ;;; Commentary:
 ;;; - https://stackoverflow.com/questions/36183071/how-can-i-preview-markdown-in-emacs-in-real-time/36189456?noredirect=1#comment104784050_36189456
 ;;; - https://wikemacs.org/wiki/Markdown#Live_preview_as_you_type
-;;; TODO: 写一个函数，完成启动服务器，启动impatient-mode, 设置filter的步骤
 ;;; Code:
 
 (maybe-require-package 'impatient-mode)
 
-(defun my-markdown-html (buffer)
+(defun spike-leung/imp-markdown-filter (buffer)
   (princ (with-current-buffer buffer
-           (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script></html>"
+           (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"https://cdn.jsdelivr.net/gh/Naereen/StrapDown.js@master/strapdown.min.js\"></script></html>"
                    (buffer-substring-no-properties (point-min) (point-max))))
          (current-buffer)))
 
-(with-eval-after-load 'impatient-mode
-  (imp-set-user-filter 'my-markdown-html))
+(defun spike-leung/preview-markdown () (interactive)
+  (progn
+    (httpd-start)
+    (impatient-mode nil)
+    (imp-set-user-filter 'spike-leung/imp-markdown-filter)))
+
+(defun spike-leung/disable-preview-markdown () (interactive)
+  (progn
+    (httpd-stop)
+    (impatient-mode -1)
+    (imp-remove-user-filter)))
 
 
 (provide 'init-my-markdown)
