d85799bac38b7144ad1b51862a742a05bba82a70
Spike <l-yanlei@hotmail.com>
2022-10-25 14:53:05 +0800

    fix: make web-mode work in vue extension

--- lisp/my-lisp/init-extension-mode.el
+++ lisp/my-lisp/init-extension-mode.el
@@ -2,7 +2,8 @@
 ;; init-extension-mode
 ;;; Commentary:
 ;;; Code:
-(add-auto-mode 'web-mode "\\.jsp\\.vue'")
+(add-auto-mode 'web-mode "\\.jsp\\'")
+(add-auto-mode 'web-mode "\\.vue\\'")
 (add-auto-mode 'js2-mode "\\.js\\'")
 
 (provide 'init-extension-mode)
