d77d886386e41e7e6228c50fa9f0278dadee33ea
Spike <l-yanlei@hotmail.com>
2023-05-25 18:23:56 +0800

    :recycle: rename vue to elgot config, remove deprecated volar config

---
+++ lisp/my-lisp/init-my-eglot.el
@@ -0,0 +1,24 @@
+;;; init-my-eglot.el --- eglot relavive config
+;;; Commentary:
+;;; Code:
+(add-hook 'emacs-startup-hook (lambda () (setq gc-cons-threshold (* 100 1024 1024))))
+(setq-default eglot-events-buffer-size 0)
+
+;;; Eglot config for Vue
+;;; see: https://emacs-china.org/t/volar-eglot/21255
+(define-derived-mode vue-mode web-mode "Vue")
+(add-to-list 'auto-mode-alist '("\\.vue\\'" . vue-mode))
+(add-hook 'vue-mode-hook 'eglot-ensure)
+
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs '(vue-mode . (eglot-volar "vue-language-server" "--stdio")))
+  (defclass eglot-volar (eglot-lsp-server) () :documentation "volar")
+  (cl-defmethod eglot-initialization-options ((server eglot-volar))
+    "Passes through required cquery initialization options"
+    `(
+      ;; Absolute path to node_modules/typescript/lib
+      :typescript (:tsdk ,(expand-file-name "~/.nvm/versions/node/v16.18.0/lib/node_modules/typescript/lib/"))
+      :fullCompletionList t
+      )))
+(provide 'init-my-eglot)
+;;; init-my-eglot.el ends here
