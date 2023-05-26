dc4d2ea37be19829c96519ce6a4c4df56435e756
Spike <l-yanlei@hotmail.com>
2023-05-26 13:39:52 +0800

    :sparkles: add yasnippet-snippets

--- lisp/my-lisp/init-yasnippet.el
+++ lisp/my-lisp/init-yasnippet.el
@@ -4,5 +4,8 @@
 (maybe-require-package 'yasnippet)
 (with-eval-after-load 'yasnippet (yas-reload-all));
 (yas-global-mode 1)
+
+;; see: https://github.com/AndreaCrotti/yasnippet-snippets
+(maybe-require-package 'yasnippet-snippets)
 (provide 'init-yasnippet)
 ;;; init-yasnippet.el ends here
