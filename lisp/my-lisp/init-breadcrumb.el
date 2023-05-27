223967f2cc6dc0d78204115c22c73d53bff7e748
Spike <l-yanlei@hotmail.com>
2023-05-27 15:05:44 +0800

    :sparkles: add breadcrumb

---
+++ lisp/my-lisp/init-breadcrumb.el
@@ -0,0 +1,8 @@
+;;; init-breadcrumb.el --- breadcrumb
+;;; Commentary:
+;;; Code:
+(push (expand-file-name "lisp/my-lisp/breadcrumb" user-emacs-directory) load-path)
+(require 'breadcrumb)
+
+(provide 'init-breadcrumb)
+;;; init-breadcrumb.el ends here
