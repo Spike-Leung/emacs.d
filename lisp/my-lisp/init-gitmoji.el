cd1d4e99310d4e99ff01a440a2820723d937e11e
Spike <l-yanlei@hotmail.com>
2023-06-02 10:27:54 +0800

    :recycle: set consult as default gitmoji-selection-backend

--- lisp/my-lisp/init-gitmoji.el
+++ lisp/my-lisp/init-gitmoji.el
@@ -1,5 +1,12 @@
+;;; init-gitmoji.el --- config gitmoji
+;;; Commentary:
+;;; Code:
 (push (expand-file-name "lisp/my-lisp/gitmoji" user-emacs-directory) load-path)
 (require 'gitmoji)
-(eval-after-load 'gitmoji
+
+(with-eval-after-load 'gitmoji
+  (setq gitmoji-selection-backend (list 'consult))
   (gitmoji-commit-mode))
+
 (provide 'init-gitmoji)
+;;; init-gitmoji.el ends here
