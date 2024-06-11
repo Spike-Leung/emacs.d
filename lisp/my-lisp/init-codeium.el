5b47e5fa36ef4e668ef46109375bade612ee6a89
Spike <l-yanlei@hotmail.com>
2024-06-11 16:27:29 +0800

    feat: :sparkles: add codeium[/]

---
+++ lisp/my-lisp/init-codeium.el
@@ -0,0 +1,35 @@
+;;; init-codeium.el --- codeium
+;;; Commentary:
+;;; Code:
+(push (expand-file-name "lisp/my-lisp/codeium" user-emacs-directory) load-path)
+(require 'codeium)
+
+(with-eval-after-load 'codeium
+  ;; (add-to-list 'completion-at-point-functions #'codeium-completion-at-point)
+
+  (setq use-dialog-box nil) ;; do not use popup boxes
+  ;; get codeium status in the modeline
+  (setq codeium-mode-line-enable
+        (lambda (api) (not (memq api '(CancelRequest Heartbeat AcceptCompletion)))))
+  (add-to-list 'mode-line-format '(:eval (car-safe codeium-mode-line)) t)
+
+  ;; use M-x codeium-diagnose to see apis/fields that would be sent to the local language server
+  (setq codeium-api-enabled
+        (lambda (api)
+          (memq api '(GetCompletions Heartbeat CancelRequest GetAuthToken RegisterUser auth-redirect AcceptCompletion))))
+
+  ;; You can overwrite all the codeium configs!
+  ;; for example, we recommend limiting the string sent to codeium for better performance
+  (defun my-codeium/document/text ()
+    (buffer-substring-no-properties (max (- (point) 3000) (point-min)) (min (+ (point) 1000) (point-max))))
+  ;; if you change the text, you should also change the cursor_offset
+  ;; warning: this is measured by UTF-8 encoded bytes
+  (defun my-codeium/document/cursor_offset ()
+    (codeium-utf8-byte-length
+     (buffer-substring-no-properties (max (- (point) 3000) (point-min)) (point))))
+  (setq codeium/document/text 'my-codeium/document/text)
+  (setq codeium/document/cursor_offset 'my-codeium/document/cursor_offset)
+  )
+
+(provide 'init-codeium)
+;;; init-codeium.el ends here
--- lisp/my-lisp/init-my-misc.el
+++ lisp/my-lisp/init-my-misc.el
@@ -14,5 +14,19 @@
          (full-url (concat jira-base-url "/browse/" issue-id)))
     (browse-url full-url)))
 
+(defvar spike-leung/js-related-modes '(vue-mode typescript-mode web-mode js-mode js2-mode js-ts-mode)
+  "List of modes to add node_modules path.")
+
+;;; enabled `add-node-modules-path` in FrontEnd related mode
+(when (maybe-require-package 'add-node-modules-path)
+  (with-eval-after-load 'flymake-flycheck
+    (with-eval-after-load 'add-node-modules-path
+      (dolist (mode spike-leung/js-related-modes)
+        (let ((mode-hook (intern (concat (symbol-name mode) "-hook"))))
+          (add-hook mode-hook 'add-node-modules-path)
+          (add-hook mode-hook (lambda ()
+                                (setq-local completion-at-point-functions '(codeium-completion-at-point))))
+          )))))
+
 (provide 'init-my-misc)
 ;;; init-my-misc.el ends here
