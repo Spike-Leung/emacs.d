7b1de8cb77b7e50fb56bbad748d81316572a11b1
Spike <l-yanlei@hotmail.com>
2024-09-24 15:29:56 +0800

    refactor: use local `emacs-immersive-translate`

--- lisp/my-lisp/init-immersive-translate.el
+++ lisp/my-lisp/init-immersive-translate.el
@@ -3,7 +3,8 @@
 ;;; Code:
 ;;; need to install https://github.com/soimort/translate-shell first.
 ;;; because `translate-shell` use google translate, emacs should able to connect to google.
-(maybe-require-package 'immersive-translate)
+(push (expand-file-name "lisp/my-lisp/emacs-immersive-translate" user-emacs-directory) load-path)
+(require 'immersive-translate)
 ;; use translate-shell
 (setq immersive-translate-backend 'trans)
 (provide 'init-immersive-translate)
