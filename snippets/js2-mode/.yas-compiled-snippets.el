6ba506da690ac5bed84ba69c298ca729a1add665
Spike <l-yanlei@hotmail.com>
2024-09-29 10:58:20 +0800

    feat: update snippet

---
+++ snippets/js2-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `js2-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'js2-mode
+                     '(("t" "$1$0: '$1'," "translate-key" nil nil nil "/home/spike/.emacs.d/snippets/js2-mode/translate-key" nil nil)))
+
+
+;;; Do not edit! File generated at Sun Sep 29 10:57:20 2024
