637c00eacd1b4e6164560581caaceeec5b849132
Spike <l-yanlei@hotmail.com>
2024-10-21 10:31:24 +0800

    Add string-inflection

---
+++ lisp/my-lisp/init-my-editing-utils.el
@@ -0,0 +1,8 @@
+;;; init-my-editing-utils.el --- Day-to-day editing helpers -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(maybe-require-package 'string-inflection)
+
+(provide 'init-my-editing-utils)
+;;; init-my-editing-utils.el ends here
