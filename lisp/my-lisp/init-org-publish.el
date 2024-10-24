b7d7aedbf0c70ecc9a25cc0d467447f876ea2ed0
Spike <l-yanlei@hotmail.com>
2024-10-24 22:48:23 +0800

    add LXGWWenKai-Bold font to org-publish common head

--- lisp/my-lisp/init-org-publish.el
+++ lisp/my-lisp/init-org-publish.el
@@ -6,15 +6,17 @@
 
 
 (defconst spike-leung/html-head "
-<link rel=\"stylesheet\" href=\"../styles/style.css\" type=\"text/css\"/>
+<link rel=\"stylesheet\" href=\"https://chinese-fonts-cdn.deno.dev/packages/lxgwwenkai/dist/LXGWWenKai-Bold/result.css\" />
 <link rel=\"stylesheet\" href=\"https://chinese-fonts-cdn.deno.dev/packages/lxgwwenkai/dist/LXGWWenKai-Regular/result.css\" />
+<link rel=\"stylesheet\" href=\"../styles/style.css\" type=\"text/css\"/>
 <link rel=\"icon\" href=\"/favicon.ico\" type=\"image/x-icon\">
 "
   "`:html-head' for `org-publish'.")
 
 (defconst spike-leung/html-head-sitemap "
-<link rel=\"stylesheet\" href=\"../styles/style.css\" type=\"text/css\"/>
+<link rel=\"stylesheet\" href=\"https://chinese-fonts-cdn.deno.dev/packages/lxgwwenkai/dist/LXGWWenKai-Bold/result.css\" />
 <link rel=\"stylesheet\" href=\"https://chinese-fonts-cdn.deno.dev/packages/lxgwwenkai/dist/LXGWWenKai-Regular/result.css\" />
+<link rel=\"stylesheet\" href=\"../styles/style.css\" type=\"text/css\"/>
 <link rel=\"stylesheet\" href=\"../styles/sitemap.css\" type=\"text/css\"/>
 <link rel=\"icon\" href=\"/favicon.ico\" type=\"image/x-icon\">
 "
