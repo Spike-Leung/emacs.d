87455104fcbc62ce0d01eed1bbf78bd0c85872cd
Spike <l-yanlei@hotmail.com>
2024-11-30 09:15:56 +0800

    :wrench: bind global key to gptel-menu

--- lisp/my-lisp/init-gptel.el
+++ lisp/my-lisp/init-gptel.el
@@ -13,8 +13,6 @@
             (error "API key not found for DeepSeek")))
       (error "No credentials found for DeepSeek"))))
 
-(spike-leung/get-deepseek-api-key)
-
 (setq gptel-model   'deepseek-chat
       gptel-backend
       (gptel-make-openai "DeepSeek"
@@ -23,6 +21,8 @@
         :stream t
         :key (spike-leung/get-deepseek-api-key) ;can be a function that returns the key
         :models '(deepseek-chat deepseek-coder)))
+
+(global-set-key (kbd "M-o g") 'gptel-menu)
 
 (provide 'init-gptel)
 ;;; init-gptel.el ends here
