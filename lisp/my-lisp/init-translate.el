eb72b335c90942e43d6082227c646b47725475d2
Spike <l-yanlei@hotmail.com>
2025-01-15 15:37:09 +0800

    :recycle: update translate setting

--- lisp/my-lisp/init-translate.el
+++ lisp/my-lisp/init-translate.el
@@ -7,35 +7,44 @@
 ;;; because `translate-shell` use google translate, emacs should able to connect to google.
 (push (expand-file-name "lisp/my-lisp/emacs-immersive-translate" user-emacs-directory) load-path)
 (require 'immersive-translate)
-;; use translate-shell
-;; (setq immersive-translate-backend 'trans)
+;; need to add deepseek api-key with user `apikey` in `.authinfo`
 (setq immersive-translate-backend 'chatgpt
-      immersive-translate-chatgpt-host "api.deepseek.com")
+      immersive-translate-chatgpt-host "api.deepseek.com"
+      immersive-translate-chatgpt-model "deepseek-chat"
+      immersive-translate-pending-message "(≖ᴗ≖๑)"
+      immersive-translate-failed-message "(つд⊂) ")
 
 
 
 ;; @see: https://github.com/condy0919/fanyi.el
 (maybe-require-package 'fanyi)
-(global-set-key (kbd "M-o f") 'fanyi-dwim2)
+
 
 
 ;; @see: https://github.com/lorniu/go-translate
 (maybe-require-package 'go-translate)
 (maybe-require-package 'plz)
 (with-eval-after-load 'go-translate
-  (setq gt-langs '(en zh))
-  (setq gt-chatgpt-key (spike-leung/get-deepseek-api-key))
-  (setq gt-chatgpt-host "https://api.deepseek.com")
-  (setq gt-chatgpt-model "deepseek-chat")
-  (setq gt-default-translator (gt-translator
+  (setq gt-langs '(en zh)
+        gt-chatgpt-key (spike-leung/get-deepseek-api-key)
+        gt-chatgpt-host "https://api.deepseek.com"
+        gt-chatgpt-model "deepseek-chat"
+        gt-default-translator (gt-translator
                                :engines (list
                                          (gt-chatgpt-engine))
                                :render (gt-buffer-render
                                         :buffer-name "gt-translator"
                                         :window-config '((display-buffer-at-bottom))
-                                        :then (lambda (_) (pop-to-buffer "gt-translator")))
-                               )))
-(global-set-key (kbd "M-o t") 'gt-do-translate)
+                                        :then (lambda (_) (pop-to-buffer "gt-translator"))))))
+
+(defvar spike-leung/my-translate-keymap (make-sparse-keymap)
+  "Keymap for translation commands.")
+
+(define-key spike-leung/my-translate-keymap (kbd "i") 'immersive-translate-buffer)
+(define-key spike-leung/my-translate-keymap (kbd "f") 'fanyi-dwim2)
+(define-key spike-leung/my-translate-keymap (kbd "g") 'gt-do-translate)
+
+(global-set-key (kbd "M-o t") spike-leung/my-translate-keymap)
 
 
 (provide 'init-translate)
