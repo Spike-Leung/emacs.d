98f35498c573091a105ab07f3d7de2c3c3ca0b2b
Spike <l-yanlei@hotmail.com>
2025-01-15 14:55:03 +0800

    :recycle: set go-translate engine to deepseek

--- lisp/my-lisp/init-translate.el
+++ lisp/my-lisp/init-translate.el
@@ -8,13 +8,15 @@
 (push (expand-file-name "lisp/my-lisp/emacs-immersive-translate" user-emacs-directory) load-path)
 (require 'immersive-translate)
 ;; use translate-shell
-(setq immersive-translate-backend 'trans)
+;; (setq immersive-translate-backend 'trans)
+(setq immersive-translate-backend 'chatgpt
+      immersive-translate-chatgpt-host "api.deepseek.com")
 
 
 
 ;; @see: https://github.com/condy0919/fanyi.el
 (maybe-require-package 'fanyi)
-
+(global-set-key (kbd "M-o f") 'fanyi-dwim2)
 
 
 ;; @see: https://github.com/lorniu/go-translate
@@ -22,9 +24,18 @@
 (maybe-require-package 'plz)
 (with-eval-after-load 'go-translate
   (setq gt-langs '(en zh))
+  (setq gt-chatgpt-key (spike-leung/get-deepseek-api-key))
+  (setq gt-chatgpt-host "https://api.deepseek.com")
+  (setq gt-chatgpt-model "deepseek-chat")
   (setq gt-default-translator (gt-translator
-                               :engines (list (gt-google-engine)))))
-
+                               :engines (list
+                                         (gt-chatgpt-engine))
+                               :render (gt-buffer-render
+                                        :buffer-name "gt-translator"
+                                        :window-config '((display-buffer-at-bottom))
+                                        :then (lambda (_) (pop-to-buffer "gt-translator")))
+                               )))
+(global-set-key (kbd "M-o t") 'gt-do-translate)
 
 
 (provide 'init-translate)
