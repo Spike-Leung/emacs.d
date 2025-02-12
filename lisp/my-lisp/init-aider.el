8d84687a98c0bfbd6deb1bfae33bb697260f39e8
Spike <l-yanlei@hotmail.com>
2025-02-12 15:15:20 +0800

    :wrench: use `Pro/deepseek-ai/DeepSeek-V3` from siliconflow in aider

--- lisp/my-lisp/init-aider.el
+++ lisp/my-lisp/init-aider.el
@@ -4,8 +4,11 @@
 (require 'aider)
 
 (with-eval-after-load 'aider
-  (setq aider-args '("--no-auto-commits" "--model" "deepseek/deepseek-coder"))
-  (setenv "DEEPSEEK_API_KEY" (spike-leung/get-deepseek-api-key))
+  ;; (setq aider-args '("--no-auto-commits" "--model" "deepseek/deepseek-chat"))
+  ;; (setenv "DEEPSEEK_API_KEY" (spike-leung/get-deepseek-api-key))
+  (setq aider-args '("--no-auto-commits" "--model" "openai/Pro/deepseek-ai/DeepSeek-V3"))
+  (setenv "OPENAI_API_KEY" (spike-leung/get-siliconflow-api-key))
+  (setenv "OPENAI_API_BASE" "https://api.siliconflow.cn")
   (global-set-key (kbd "M-o a") 'aider-transient-menu))
 
 (provide 'init-aider)
