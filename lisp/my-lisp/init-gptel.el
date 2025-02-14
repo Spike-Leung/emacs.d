3344d3bbd7a77ce960e37b8f9ce8b20fdf26f8d0
Spike <l-yanlei@hotmail.com>
2025-02-14 13:58:26 +0800

    :recycle: set SiliconFlow deepseek V3 as default gptel model

--- lisp/my-lisp/init-gptel.el
+++ lisp/my-lisp/init-gptel.el
@@ -4,6 +4,7 @@
 (maybe-require-package 'gptel)
 
 (with-eval-after-load 'gptel
+  ;; define provider
   (gptel-make-openai "DeepSeek"
     :host "api.deepseek.com"
     :endpoint "/chat/completions"
@@ -19,15 +20,26 @@
               Pro/deepseek-ai/DeepSeek-V3
               deepseek-ai/DeepSeek-R1-Distill-Qwen-32B
               deepseek-ai/DeepSeek-R1-Distill-Llama-70B))
+  (gptel-make-openai "DeepSeek"
+    :host "api.deepseek.com"
+    :endpoint "/chat/completions"
+    :stream t
+    :key (spike-leung/get-deepseek-api-key)
+    :models '(deepseek-reasoner deepseek-chat deepseek-coder))
   (gptel-make-gemini "Gemini" :key (spike-leung/get-gemini-api-key) :stream t)
+  ;; set default
   (setq
-   gptel-model 'deepseek-reasoner
-   gptel-backend (gptel-make-openai "DeepSeek"
-                   :host "api.deepseek.com"
+   gptel-model "Pro/deepseek-ai/DeepSeek-V3"
+   gptel-backend (gptel-make-openai "SiliconFlow"
+                   :host "api.siliconflow.cn"
                    :endpoint "/chat/completions"
                    :stream t
-                   :key (spike-leung/get-deepseek-api-key)
-                   :models '(deepseek-reasoner deepseek-chat deepseek-coder))))
+                   :key (spike-leung/get-siliconflow-api-key)
+                   :models '(Pro/deepseek-ai/DeepSeek-R1
+                             Pro/deepseek-ai/DeepSeek-V3
+                             deepseek-ai/DeepSeek-R1-Distill-Qwen-32B
+                             deepseek-ai/DeepSeek-R1-Distill-Llama-70B))
+   ))
 
 (global-set-key (kbd "M-o g") 'gptel-menu)
 
