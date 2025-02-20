f73b416079b876f48228073fbcbace413def4a49
Spike <l-yanlei@hotmail.com>
2025-02-20 17:07:06 +0800

    :wrench: gtpel use openrouter as default

--- lisp/my-lisp/init-gptel.el
+++ lisp/my-lisp/init-gptel.el
@@ -3,6 +3,26 @@
 
 (maybe-require-package 'gptel)
 
+(defvar spike-leung/openrouter-models
+  '(google/gemini-2.0-flash-001
+    google/gemini-flash-1.5-8b
+    openai/gpt-4o-mini
+    openai/gpt-4o
+    anthropic/claude-3.5-sonnet
+    deepseek/deepseek-r1:free
+    deepseek/deepseek-r1
+    deepseek/deepseek-chat
+    deepseek/deepseek-chat:free
+    qwen/qwen-2.5-72b-instruct)
+  "List of available models for OpenRouter API.")
+
+(defvar spike-leung/siliconflow-models
+  '(Pro/deepseek-ai/DeepSeek-R1
+    Pro/deepseek-ai/DeepSeek-V3
+    deepseek-ai/DeepSeek-R1-Distill-Qwen-32B
+    deepseek-ai/DeepSeek-R1-Distill-Llama-70B)
+  "List of available models for SiliconFlow API.")
+
 (with-eval-after-load 'gptel
   ;; define provider
   (gptel-make-openai "DeepSeek"
@@ -16,30 +36,23 @@
     :endpoint "/chat/completions"
     :stream t
     :key (spike-leung/get-siliconflow-api-key)
-    :models '(Pro/deepseek-ai/DeepSeek-R1
-              Pro/deepseek-ai/DeepSeek-V3
-              deepseek-ai/DeepSeek-R1-Distill-Qwen-32B
-              deepseek-ai/DeepSeek-R1-Distill-Llama-70B))
-  (gptel-make-openai "DeepSeek"
-    :host "api.deepseek.com"
-    :endpoint "/chat/completions"
+    :models spike-leung/siliconflow-models)
+  (gptel-make-openai "OpenRouter"       ;Any name you want
+    :host "openrouter.ai"
+    :endpoint "/api/v1/chat/completions"
     :stream t
-    :key (spike-leung/get-deepseek-api-key)
-    :models '(deepseek-reasoner deepseek-chat deepseek-coder))
+    :key (spike-leung/get-openrouter-api-key)
+    :models spike-leung/openrouter-models)
   (gptel-make-gemini "Gemini" :key (spike-leung/get-gemini-api-key) :stream t)
   ;; set default
-  (setq
-   gptel-model "Pro/deepseek-ai/DeepSeek-V3"
-   gptel-backend (gptel-make-openai "SiliconFlow"
-                   :host "api.siliconflow.cn"
-                   :endpoint "/chat/completions"
-                   :stream t
-                   :key (spike-leung/get-siliconflow-api-key)
-                   :models '(Pro/deepseek-ai/DeepSeek-R1
-                             Pro/deepseek-ai/DeepSeek-V3
-                             deepseek-ai/DeepSeek-R1-Distill-Qwen-32B
-                             deepseek-ai/DeepSeek-R1-Distill-Llama-70B))
-   ))
+  (setq gptel-model   'google/gemini-2.0-flash-001
+        gptel-backend
+        (gptel-make-openai "OpenRouter"
+          :host "openrouter.ai"
+          :endpoint "/api/v1/chat/completions"
+          :stream t
+          :key (spike-leung/get-openrouter-api-key)
+          :models spike-leung/openrouter-models)))
 
 (global-set-key (kbd "M-o g") 'gptel-menu)
 
