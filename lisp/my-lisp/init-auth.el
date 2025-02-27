9f05e12d32797ac0f77c1aeea20391a496803e0a
Spike <l-yanlei@hotmail.com>
2025-02-27 10:21:49 +0800

    :recycle: add init-auth to config auth related things

---
+++ lisp/my-lisp/init-auth.el
@@ -0,0 +1,38 @@
+;;; init-auth --- config for auth related
+;;; Commentary:
+;;; Code:
+(setq epa-pinentry-mode 'loopback) ;; input pass in minibuffer
+(auth-source-pass-enable)
+
+;;; function to get api-key from authinfo
+(defun spike-leung/get-api-key (service &optional host)
+  "Retrieve the API key for SERVICE from authinfo.  if HOST exist, retrieve by HOST."
+  (let* ((host (or host (format "api.%s.com" service)))
+         (creds (car (auth-source-search :host host :port 443))))
+    (if creds
+        (let ((api-key (plist-get creds :secret
+                                  )))
+          (if (functionp api-key)
+              (funcall api-key)
+            api-key
+            (error "API key not found for %s" service)))
+      (error "No credentials found for %s" service))))
+
+(defun spike-leung/get-deepseek-api-key ()
+  "Retrieve the DeepSeek API key from authinfo."
+  (spike-leung/get-api-key "deepseek"))
+
+(defun spike-leung/get-siliconflow-api-key ()
+  "Retrieve the SiliconFlow API key from authinfo."
+  (spike-leung/get-api-key "siliconflow" "api.siliconflow.cn"))
+
+(defun spike-leung/get-openrouter-api-key ()
+  "Retrieve the OpenRouter API key from authinfo."
+  (spike-leung/get-api-key "openrouter" "openrouter.ai"))
+
+(defun spike-leung/get-gemini-api-key ()
+  "Retrieve the Gemini API key from authinfo."
+  (spike-leung/get-api-key "gemini"))
+
+(provide 'init-auth)
+;;; init-auth.el ends here.
