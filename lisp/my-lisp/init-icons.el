26fafccd01d13774becf33a5380d9854604fa6f8
Spike <l-yanlei@hotmail.com>
2025-03-05 09:50:47 +0800

    :recycle: nerd-icons-dired & nerd-icons-ibuffer should load after nerd-icons

--- lisp/my-lisp/init-icons.el
+++ lisp/my-lisp/init-icons.el
@@ -2,12 +2,11 @@
 ;;; Commentary:
 ;;; Code:
 
-(maybe-require-package 'nerd-icons)
-(maybe-require-package 'nerd-icons-dired)
-(maybe-require-package 'nerd-icons-ibuffer)
-
-(add-hook 'dired-mode-hook #'nerd-icons-dired-mode)
-(add-hook 'ibuffer-mode-hook #'nerd-icons-ibuffer-mode)
+(when (maybe-require-package 'nerd-icons)
+  (maybe-require-package 'nerd-icons-dired)
+  (maybe-require-package 'nerd-icons-ibuffer)
+  (add-hook 'dired-mode-hook #'nerd-icons-dired-mode)
+  (add-hook 'ibuffer-mode-hook #'nerd-icons-ibuffer-mode))
 
 (setq magit-format-file-function 'magit-format-file-nerd-icons)
 
