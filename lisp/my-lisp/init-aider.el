3e4ce5e7e8a8a4a1ff0f37045b0aecf727a38fc7
Spike <l-yanlei@hotmail.com>
2025-03-05 18:25:14 +0800

    :wrench: update aider default model _(:3 」∠ )_

--- lisp/my-lisp/init-aider.el
+++ lisp/my-lisp/init-aider.el
@@ -8,7 +8,7 @@
   (setenv "OPENAI_API_KEY" (spike-leung/get-siliconflow-api-key))
   (setenv "OPENAI_API_BASE" "https://api.siliconflow.cn")
   (setenv "OPENROUTER_API_KEY" (spike-leung/get-openrouter-api-key))
-  (setq aider-args '("--no-auto-commits" "--model" "openrouter/anthropic/claude-3.7-sonnet"))
+  (setq aider-args '("--no-auto-commits" "--model" "deepseek/deepseek-chat"))
   (setq aider-popular-models '(
                                "openrouter/anthropic/claude-3.7-sonnet"
                                "deepseek/deepseek-chat"
