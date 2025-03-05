7a1ff4a80d4716728c8a8db56ab01a3bd76ac61f
Spike <l-yanlei@hotmail.com>
2025-03-05 14:36:37 +0800

    :recycle: fix warning: Overwriting value of ‘denote-user-enforced-denote-directory’ by aliasing to ‘denote-directory’

--- lisp/my-lisp/init-denote.el
+++ lisp/my-lisp/init-denote.el
@@ -46,7 +46,9 @@
           'grep)))
 
   (defvar my-denote-silo-directories
-    `("~/notes/.hidden" "~/notes/.privacy" "~/notes/gptel")
+    `("~/notes/.hidden"
+      "~/notes/.privacy"
+      "~/notes/gptel")
     "List of file paths pointing to my Denote silos.
 This is a list of strings.")
 
@@ -65,7 +67,7 @@ COMMAND is one among `my-denote-commands-for-silos'."
            (intern (completing-read
                     "Run command in silo: "
                     my-denote-commands-for-silos nil t))))
-    (let ((denote-user-enforced-denote-directory silo))
+    (let ((denote-directory silo))
       (call-interactively command)))
 
   ;; Denote DOES NOT define any key bindings.  This is for the user to
