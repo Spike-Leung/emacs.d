f681d3b48711cb9f3f78583a77cd3e9821ceea56
Spike <l-yanlei@hotmail.com>
2025-03-07 01:06:10 +0800

    :wrench: add view-transition-name to index href

--- lisp/my-lisp/init-org-publish.el
+++ lisp/my-lisp/init-org-publish.el
@@ -49,7 +49,7 @@
 " spike-leung/html-preamble)
   "`:html-preamble' for `org-publish'.Customize for content." )
 
-(defconst spike-lenng/html-postamble "
+(defconst spike-leung/html-postamble "
 <p class=\"author\">Author: <a href=\"mailto:l-yanlei@hotmail.com\">%a</a></p>
 <p class=\"date\">Date: %d</p>
 <p class=\"date\">Last Modified: %C</p>
@@ -167,6 +167,62 @@ TITLE is the RSS feed title and LIST contains files to include."
    (org-list-to-subtree list)))
 
 
+(defun spike-leung/org-html-publish-common (plist filename pub-dir)
+  "Common HTML publishing logic for org files and sitemap.
+PLIST is the property list for the given project.
+FILENAME is the org file being published.
+PUB-DIR is the publishing directory.
+Returns the path to the generated HTML file."
+  (let ((org-html-link-org-files-as-html t))
+    ;; First publish to HTML using org-mode's standard publishing function
+    (org-html-publish-to-html plist filename pub-dir)
+    ;; Return the path to the generated HTML file
+    (concat pub-dir "/" (file-name-nondirectory (file-name-sans-extension filename)) ".html")))
+
+(defun spike-leung/get-view-transition-name (filename)
+  "Generate consistent view-transition-name based on FILENAME.
+Strips directory and .org extension, and converts to valid CSS identifier."
+  (let ((basename (file-name-base filename)))
+    (replace-regexp-in-string "[^a-zA-Z0-9-]" "-" basename)))
+
+(defun spike-leung/org-html-publish-to-html-orgfiles (plist filename pub-dir)
+  "Publish FILENAME to HTML with view-transition-name added to title.
+PLIST is the property list for the given project.
+FILENAME is the org file being published.
+PUB-DIR is the publishing directory."
+  (let ((html-file (spike-leung/org-html-publish-common plist filename pub-dir)))
+    (when (file-exists-p html-file)
+      (with-temp-buffer
+        (insert-file-contents html-file)
+        (goto-char (point-min))
+        ;; Add view-transition-name to <h1> title tags for smooth page transitions
+        (while (re-search-forward "<h1[^>]*>\\([^<]+\\)</h1>" nil t)
+          (let ((title (match-string 1)))
+            (replace-match (format "<h1 style=\"view-transition-name: spike-%s\" class=\"title\">%s</h1>"
+                                   (spike-leung/get-view-transition-name filename)
+                                   title))))
+        (write-region (point-min) (point-max) html-file)))))
+
+(defun spike-leung/org-html-publish-to-html-sitemap (plist filename pub-dir)
+  "Publish FILENAME to HTML with view-transition-name added to links.
+PLIST is the property list for the given project.
+FILENAME is the org file being published (typically index.org).
+PUB-DIR is the publishing directory."
+  (let ((html-file (spike-leung/org-html-publish-common plist filename pub-dir)))
+    (when (file-exists-p html-file)
+      (with-temp-buffer
+        (insert-file-contents html-file)
+        (goto-char (point-min))
+        ;; Add view-transition-name to <a> tags linking to content pages
+        ;; This enables smooth transitions when navigating from sitemap to content
+        (while (re-search-forward "<a href=\"\\([^\"]+\\)\"" nil t)
+          (let* ((href (match-string 1))
+                 (transition-name (spike-leung/get-view-transition-name href)))
+            (replace-match (format "<a href=\"%s\" style=\"view-transition-name: spike-%s\""
+                                   href
+                                   transition-name))))
+        (write-region (point-min) (point-max) html-file)))))
+
 (defun rw/format-rss-feed-entry (entry style project)
   "Format ENTRY for the RSS feed.
 ENTRY is a file name.  STYLE is either 'list' or 'tree'.
@@ -191,14 +247,14 @@ PROJECT is the current project."
          :base-directory "~/git/taxodium/post"
          :base-extension "org"
          :publishing-directory "~/git/taxodium/publish"
-         :publishing-function org-html-publish-to-html
+         :publishing-function spike-leung/org-html-publish-to-html-orgfiles
          :section-numbers nil
          :with-toc t
          :with-tags t
          :time-stamp-file nil
          :html-head ,spike-leung/html-head
          :html-preamble ,spike-leung/html-preamble-content
-         :html-postamble ,spike-lenng/html-postamble
+         :html-postamble ,spike-leung/html-postamble
          :exclude "rss.org"
          :auto-sitemap t
          :sitemap-filename "index.org"
@@ -213,7 +269,7 @@ PROJECT is the current project."
          :base-directory "~/git/taxodium/post"
          :base-extension "org"
          :publishing-directory "~/git/taxodium/publish"
-         :publishing-function org-html-publish-to-html
+         :publishing-function spike-leung/org-html-publish-to-html-sitemap
          :time-stamp-file nil
          :html-head ,spike-leung/html-head-sitemap
          :html-preamble ,spike-leung/html-preamble
