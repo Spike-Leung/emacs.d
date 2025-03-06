1c58c5b284c00f09f5eb1f4fbe49d142ea6f7269
Spike <l-yanlei@hotmail.com>
2025-03-06 15:10:13 +0800

    :wrench: add more model for aider

--- lisp/my-lisp/init-aider.el
+++ lisp/my-lisp/init-aider.el
@@ -10,11 +10,15 @@
     (setenv "OPENAI_API_BASE" "https://api.siliconflow.cn")
     (setenv "OPENROUTER_API_KEY" (spike-leung/get-openrouter-api-key))
     (setq aider-args '("--no-auto-commits" "--model" "deepseek/deepseek-chat"))
-    (setq aider-popular-models '(
-                                 "openrouter/anthropic/claude-3.7-sonnet"
+    (setq aider-popular-models '("openrouter/anthropic/claude-3.7-sonnet"
+                                 "openrouter/deepseek/deepseek-r1"
+                                 "openrouter/openai/gpt-4o"
+                                 "openrouter/openai/gpt-4o-mini"
+                                 "openrouter/google/gemini-2.0-flash-001"
+                                 ;; deepseek official
                                  "deepseek/deepseek-chat"
-                                 "openai/Pro/deepseek-ai/DeepSeek-V3" ;; provide by siliconflow))
-                                 ))
+                                 ;; provide by siliconflow
+                                 "openai/Pro/deepseek-ai/DeepSeek-V3"))
     (global-set-key (kbd "M-o a") 'aider-transient-menu)))
 
 
