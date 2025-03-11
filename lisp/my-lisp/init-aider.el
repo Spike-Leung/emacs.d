9d31ad9898cf2de6e3e6c214f0e23fcf5035d457
Spike <l-yanlei@hotmail.com>
2025-03-11 14:10:20 +0800

    :wrench: use gemini-2.0-flash-001 as default aider model

--- lisp/my-lisp/init-aider.el
+++ lisp/my-lisp/init-aider.el
@@ -9,9 +9,10 @@
     (setenv "OPENAI_API_KEY" (spike-leung/get-siliconflow-api-key))
     (setenv "OPENAI_API_BASE" "https://api.siliconflow.cn")
     (setenv "OPENROUTER_API_KEY" (spike-leung/get-openrouter-api-key))
-    (setq aider-args '("--no-auto-commits" "--model" "openrouter/qwen/qwq-32b"))
+    (setq aider-args '("--no-auto-commits" "--model" "openrouter/google/gemini-2.0-flash-001"))
     (setq aider-popular-models '("openrouter/anthropic/claude-3.7-sonnet"
                                  "openrouter/deepseek/deepseek-r1"
+                                 "openrouter/deepseek/deepseek-chat"
                                  "openrouter/openai/gpt-4o"
                                  "openrouter/openai/gpt-4o-mini"
                                  "openrouter/google/gemini-2.0-flash-001"
