c2e222a6cff3919c905f515c0ae7125ac34df2fb
Spike <l-yanlei@hotmail.com>
2025-03-17 09:30:11 +0800

    :recycle: require aider from melpa

--- lisp/my-lisp/init-aider.el
+++ lisp/my-lisp/init-aider.el
@@ -1,28 +1,32 @@
 ;;; init-aider.el --- aider -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
-(require 'aider)
 
-(with-eval-after-load 'init-auth
-  (with-eval-after-load 'aider
-    (setenv "DEEPSEEK_API_KEY" (spike-leung/get-deepseek-api-key))
-    (setenv "OPENAI_API_KEY" (spike-leung/get-siliconflow-api-key))
-    (setenv "OPENAI_API_BASE" "https://api.siliconflow.cn")
-    (setenv "OPENROUTER_API_KEY" (spike-leung/get-openrouter-api-key))
-    (setq aider-args '("--no-auto-commits" "--model" "openrouter/anthropic/claude-3.7-sonnet"))
-    (setq aider-popular-models '("openrouter/anthropic/claude-3.7-sonnet"
-                                 "openrouter/deepseek/deepseek-r1"
-                                 "openrouter/deepseek/deepseek-chat"
-                                 "openrouter/openai/gpt-4o"
-                                 "openrouter/openai/gpt-4o-mini"
-                                 "openrouter/google/gemini-2.0-flash-001"
-                                 "openrouter/qwen/qwq-32b"
-                                 ;; deepseek official
-                                 "deepseek/deepseek-chat"
-                                 ;; provide by siliconflow
-                                 "openai/Pro/deepseek-ai/DeepSeek-V3"))
-    (global-set-key (kbd "M-o a") 'aider-transient-menu)))
+(eval-after-load 'aider (message "aider"))
 
+(when (maybe-require-package 'aider)
+  (with-eval-after-load 'init-auth
+    ;; `with-eval-after-load' 是首次加载时才会执行 body，如果已经加载过了，就不会执行 body
+    ;; `eval-after-load' 无关是否首次加载
+    (eval-after-load 'aider
+      (progn
+        (setenv "DEEPSEEK_API_KEY" (spike-leung/get-deepseek-api-key))
+        (setenv "OPENAI_API_KEY" (spike-leung/get-siliconflow-api-key))
+        (setenv "OPENAI_API_BASE" "https://api.siliconflow.cn")
+        (setenv "OPENROUTER_API_KEY" (spike-leung/get-openrouter-api-key))
+        (setq aider-args '("--no-auto-commits" "--model" "openrouter/anthropic/claude-3.7-sonnet"))
+        (setq aider-popular-models '("openrouter/anthropic/claude-3.7-sonnet"
+                                     "openrouter/deepseek/deepseek-r1"
+                                     "openrouter/deepseek/deepseek-chat"
+                                     "openrouter/openai/gpt-4o"
+                                     "openrouter/openai/gpt-4o-mini"
+                                     "openrouter/google/gemini-2.0-flash-001"
+                                     "openrouter/qwen/qwq-32b"
+                                     ;; deepseek official
+                                     "deepseek/deepseek-chat"
+                                     ;; provide by siliconflow
+                                     "openai/Pro/deepseek-ai/DeepSeek-V3"))
+        (global-set-key (kbd "M-o a") 'aider-transient-menu)))))
 
 (provide 'init-aider)
 ;;; init-aider.el ends here
