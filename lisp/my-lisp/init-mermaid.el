160a9f407823dff36b8580a83f66410e9f1b4a61
Spike <l-yanlei@hotmail.com>
2025-04-11 14:54:27 +0800

    :sparkles: add function to live preview mermaid compiled png

--- lisp/my-lisp/init-mermaid.el
+++ lisp/my-lisp/init-mermaid.el
@@ -11,5 +11,14 @@
 (maybe-require-package 'mermaid-mode)
 (add-to-list 'auto-mode-alist '("\\.mmd\\'" . mermaid-mode))
 
+(when *is-wsl*
+  (defun spike-leung/open-png-with-feh ()
+    "Prompt for a PNG file and open it with 'feh --reload 1'."
+    (interactive)
+    (let* ((file (read-file-name "Select PNG file: " nil nil t nil
+                                 (lambda (f) (string-match-p "\\.png\\'" f)))))
+      (when (and file (file-exists-p file) (string-match-p "\\.png\\'" file))
+        (start-process "feh" nil "feh" "--reload" "1" file)))))
+
 (provide 'init-mermaid)
 ;;; init-mermaid.el ends here
