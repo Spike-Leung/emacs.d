709b9c448ec54ac40f59761cc9bfa7c3538d43b7
Spike <l-yanlei@hotmail.com>
2025-04-16 14:28:24 +0800

    :sparkles: add consult-line-symbol-at-point

--- lisp/my-lisp/init-my-editing-utils.el
+++ lisp/my-lisp/init-my-editing-utils.el
@@ -7,5 +7,15 @@
 
 (global-set-key (kbd "C-x u") 'vundo)
 
+;; see: https://arialdomartini.github.io/consult-line-at-point
+(defun spike-leung/consult-line-symbol-at-point ()
+  "Search for a line matching the symbol found near point."
+  (interactive)
+  (consult-line
+   (or (thing-at-point 'symbol))))
+
+(global-set-key (kbd "M-s .") #'spike-leung/consult-line-symbol-at-point)
+(global-set-key (kbd "M-s M-s .") #'isearch-forward-symbol-at-point)
+
 (provide 'init-my-editing-utils)
 ;;; init-my-editing-utils.el ends here
