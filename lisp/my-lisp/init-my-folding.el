205b0e1c27be03be1ea15ad808278a1845d1e4c4
Spike <l-yanlei@hotmail.com>
2025-04-23 17:34:30 +0800

    :sparkles: add more treesit-fold keybinding

--- lisp/my-lisp/init-my-folding.el
+++ lisp/my-lisp/init-my-folding.el
@@ -7,7 +7,9 @@
   (global-treesit-fold-indicators-mode 1)
   (with-eval-after-load 'treesit-fold
     (define-key treesit-fold-mode-map (kbd "C-c f") #'treesit-fold-toggle)
-    (define-key treesit-fold-mode-map (kbd "C-c F") #'treesit-fold-close-all)))
+    (define-key treesit-fold-mode-map (kbd "C-c F") #'treesit-fold-open-recursively)
+    (define-key treesit-fold-mode-map (kbd "C-c o") #'treesit-fold-close-all)
+    (define-key treesit-fold-mode-map (kbd "C-c O") #'treesit-fold-open-all)))
 
 (provide 'init-my-folding)
 ;;; init-my-folding.el ends here
