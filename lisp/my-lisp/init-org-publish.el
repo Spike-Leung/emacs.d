ff5d5fae32f0a4b0721104096bdb59f05f8f8a3e
Spike <l-yanlei@hotmail.com>
2025-04-27 23:51:32 +0800

    :wrench: update blog giscus theme

--- lisp/my-lisp/init-org-publish.el
+++ lisp/my-lisp/init-org-publish.el
@@ -69,8 +69,8 @@
         data-reactions-enabled=\"1\"
         data-emit-metadata=\"0\"
         data-input-position=\"top\"
-        data-theme=\"preferred_color_scheme\"
-        data-lang=\"en\"
+        data-theme=\"https://taxodium.ink/styles/giscus/preferred-color-scheme.css\"
+        data-lang=\"zh-CN\"
         data-loading=\"lazy\"
         crossorigin=\"anonymous\"
         async>
