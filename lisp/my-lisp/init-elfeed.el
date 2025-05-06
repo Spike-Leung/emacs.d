74d861e08fd185b4fd0f84d8792aa2ba676a6380
Spike <l-yanlei@hotmail.com>
2025-05-06 15:37:11 +0800

    :wrench: do elfeed-db-compact when exit emacs

--- lisp/my-lisp/init-elfeed.el
+++ lisp/my-lisp/init-elfeed.el
@@ -7,5 +7,7 @@
     (setq rmh-elfeed-org-files (list "~/.emacs.d/elfeed.org"))
     (elfeed-org)))
 
+(add-hook 'kill-emacs-hook #'elfeed-db-compact)
+
 (provide 'init-elfeed)
 ;;; init-elfeed.el ends here
