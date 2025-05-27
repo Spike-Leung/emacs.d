6d3e2734eed063d2e5475de77c32b5b25bf7cefb
Spike <l-yanlei@hotmail.com>
2025-05-27 14:53:25 +0800

    :recycle: require aider to ensure aider-transient-menu-2cols loaded

--- lisp/my-lisp/init-aider.el
+++ lisp/my-lisp/init-aider.el
@@ -2,22 +2,23 @@
 ;;; Commentary:
 ;;; Code:
 (require 'init-openrouter-models)
+(maybe-require-package 'aider)
+(require 'aider)
 
-(when (maybe-require-package 'aider)
-  (with-eval-after-load 'init-auth
-    ;; `with-eval-after-load' 是首次加载时才会执行 body，如果已经加载过了，就不会执行 body
-    ;; `eval-after-load' 无关是否首次加载
-    (eval-after-load 'aider
-      (progn
-        (setenv "DEEPSEEK_API_KEY" (spike-leung/get-deepseek-api-key))
-        (setenv "OPENAI_API_KEY" (spike-leung/get-siliconflow-api-key))
-        (setenv "OPENAI_API_BASE" "https://api.siliconflow.cn")
-        (setenv "OPENROUTER_API_KEY" (spike-leung/get-openrouter-api-key))
-        (setq aider-args '("--no-auto-commits" "--model" "openrouter/google/gemini-2.5-pro-preview"))
-        ;; Use advice to lazy-load models before menu
-        (advice-add 'aider-transient-menu :before #'spike-leung/aider-ensure-models)))
+(with-eval-after-load 'init-auth
+  ;; `with-eval-after-load' 是首次加载时才会执行 body，如果已经加载过了，就不会执行 body
+  ;; `eval-after-load' 无关是否首次加载
+  (eval-after-load 'aider
+    (progn
+      (setenv "DEEPSEEK_API_KEY" (spike-leung/get-deepseek-api-key))
+      (setenv "OPENAI_API_KEY" (spike-leung/get-siliconflow-api-key))
+      (setenv "OPENAI_API_BASE" "https://api.siliconflow.cn")
+      (setenv "OPENROUTER_API_KEY" (spike-leung/get-openrouter-api-key))
+      (setq aider-args '("--no-auto-commits" "--model" "openrouter/google/gemini-2.5-pro-preview"))
+      ;; Use advice to lazy-load models before menu
+      (advice-add 'aider-transient-menu-2cols :before #'spike-leung/aider-ensure-models)))
 
-    (global-set-key (kbd "M-o a") 'aider-transient-menu-2cols)))
+  (global-set-key (kbd "M-o a") 'aider-transient-menu-2cols))
 
 (defun spike-leung/aider-ensure-models (&rest _args)
   "Set `aider-popular-models` from OpenRouter models cache."
