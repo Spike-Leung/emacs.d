e9ab5160ea842c29e0f1c78cb0594bd9ce3ae56a
Spike <l-yanlei@hotmail.com>
2025-06-20 12:55:30 +0800

    :sparkles: live preview markdown with GitHub markdown style
    :sparkles: live preview markdown with GitHub markdown style

--- lisp/my-lisp/init-my-markdown.el
+++ lisp/my-lisp/init-my-markdown.el
@@ -1,22 +1,50 @@
 ;;; init-my-markdown.el --- use impatient-mode to preview markdown
 ;;; Commentary:
-;;; - https://stackoverflow.com/questions/36183071/how-can-i-preview-markdown-in-emacs-in-real-time/36189456?noredirect=1#comment104784050_36189456
-;;; - https://wikemacs.org/wiki/Markdown#Live_preview_as_you_type
-;;; TODO: use github markdown style
+;;; @see:  https://wikemacs.org/wiki/Markdown#Live_preview_as_you_type
 ;;; Code:
 
 (maybe-require-package 'impatient-mode)
 
 (defun spike-leung/imp-markdown-filter (buffer)
-  "Define imp markdown filter.Wrap BUFFER with 'strapdown.min.js'."
+  "Define imp markdown filter.
+Wrap BUFFER with HTML, render with https://github.com/markedjs/marked and style with https://github.com/sindresorhus/github-markdown-css"
   (princ (with-current-buffer buffer
-           (format "<!DOCTYPE html>
+(format "<!DOCTYPE html>
 <html>
-<title>Impatient Markdown</title>
-<xmp theme=\"lumen\" style=\"display:none;\">%s</xmp>
-<script src=\"https://cdn.jsdelivr.net/gh/Naereen/StrapDown.js@master/strapdown.min.js\"></script>
+  <title>Markdown Preview</title>
+  <head>
+    <link
+      rel=\"stylesheet\"
+      href=\"https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.8.1/github-markdown.min.css\"
+      integrity=\"sha512-BrOPA520KmDMqieeM7XFe6a3u3Sb3F1JBaQnrIAmWg3EYrciJ+Qqe6ZcKCdfPv26rGcgTrJnZ/IdQEct8h3Zhw==\"
+      crossorigin=\"anonymous\"
+      referrerpolicy=\"no-referrer\"
+    />
+    <style>
+          .markdown-body {
+                  box-sizing: border-box;
+                  min-width: 200px;
+                  max-width: 980px;
+                  margin: 0 auto;
+                  padding: 45px;
+          }
+
+          @media (max-width: 767px) {
+                  .markdown-body {
+                          padding: 15px;
+                  }
+          }
+    </style>
+    <script src=\"https://cdn.jsdelivr.net/npm/marked/marked.min.js\"></script>
+  </head>
+  <body>
+    <div id=\"markdown-body\" class=\"markdown-body\"></div>
+    <script>
+      document.getElementById('markdown-body').innerHTML = marked.parse(%s);
+    </script>
+  </body>
 </html>"
-                   (buffer-substring-no-properties (point-min) (point-max))))
+                   (json-encode (buffer-substring-no-properties (point-min) (point-max)))))
          (current-buffer)))
 
 (defun spike-leung/preview-markdown ()
