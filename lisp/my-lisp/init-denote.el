1d93ec424b55c9e52517477c5cfaed05bd08267b
Spike <l-yanlei@hotmail.com>
2025-08-28 17:12:22 +0800

    :wrench: update denote setting

--- lisp/my-lisp/init-denote.el
+++ lisp/my-lisp/init-denote.el
@@ -8,71 +8,69 @@
 (when (maybe-require-package 'denote)
   ;; Remember to check the doc strings of those variables.
   (setq denote-directory (expand-file-name "~/notes/"))
-  (setq denote-known-keywords '("emacs" "frontend"))
+  (setq denote-silo-directories
+        (list denote-directory
+              "~/git/taxodium/posts"))
+  (setq denote-known-keywords '("emacs" "frontend" "writing" "blog" "draft"))
   (setq denote-infer-keywords t)
   (setq denote-sort-keywords t)
   (setq denote-file-type nil)    ; Org is the default, set others here
   (setq denote-prompts '(template title keywords))
   (setq denote-excluded-directories-regexp nil)
   (setq denote-excluded-keywords-regexp nil)
+  (setq denote-excluded-files-regexp "hidden")
 
   ;; Pick dates, where relevant, with Org's advanced interface:
   (setq denote-date-prompt-use-org-read-date t)
-
-  ;; Read manual for how to specify `denote-templates'.  We do not
-  ;; include an example here to avoid potential confusion.
-  (setq denote-templates
-        '((memo . "* Refs")))
 
   ;; We do not allow multi-word keywords by default.  The author's
   ;; personal preference is for single-word keywords for a more rigid
   ;; workflow.
   (setq denote-allow-multi-word-keywords t)
-
   (setq denote-date-format nil)         ; read doc string
 
   ;; By default, we do not show the context of links.  We just display
   ;; file names.  This provides a more informative view.
   (setq denote-backlinks-show-context t)
 
+  ;; When this user option is set to a non-nil value, Denote will use minibuffer history entries as completion candidates in all of the `denote-prompts-with-history-as-completion'.
+  (setq denote-history-completion-in-prompts t)
+
+  ;; Read manual for how to specify `denote-templates'.  We do not
+  ;; include an example here to avoid potential confusion.
+  (setq denote-templates
+        '((memo . "* Refs")))
+
   (setq xref-search-program
         (cond
-          ((or (executable-find "ripgrep")
-               (executable-find "rg"))
-           'ripgrep)
-          ((executable-find "ugrep")
-           'ugrep)
-          (t
-           'grep)))
-  (setq denote-silo-directories
-        (list denote-directory
-              "~/notes/.private"
-              "~/notes/gptel"
-              "~/git/taxodium/posts"))
+         ((or (executable-find "ripgrep")
+              (executable-find "rg"))
+          'ripgrep)
+         ((executable-find "ugrep")
+          'ugrep)
+         (t
+          'grep)))
 
-  (defvar my-denote-silo-directories
-    `("~/notes/.private"
-      "~/notes/gptel"
-      "~/git/taxodium/posts")
-    "List of file paths pointing to my Denote silos.
-This is a list of strings.")
-
-;;; Denote key bindings.
+  ;;; Denote key bindings.
   (let ((map global-map))
     (define-key map (kbd "C-c n n") #'denote-open-or-create)
     (define-key map (kbd "C-c n N") #'denote-silo-open-or-create)
+    (define-key map (kbd "C-c n i") #'denote-link-or-create) ; "insert" mnemonic
+    (define-key map (kbd "C-c n I") #'denote-add-links)
+    (define-key map (kbd "C-c n b") #'denote-backlinks)
+    (define-key map (kbd "C-c n g") #'denote-grep)
     (define-key map (kbd "C-c n c") #'denote-region) ; "contents" mnemonic
     (define-key map (kbd "C-c n s") #'denote-subdirectory)
     (define-key map (kbd "C-c n t") #'denote-template)
     (define-key map (kbd "C-c n k a") #'denote-keywords-add)
     (define-key map (kbd "C-c n k k") #'denote-keywords-remove)
-    (define-key map (kbd "C-c n i") #'denote-link-or-create) ; "insert" mnemonic
-    (define-key map (kbd "C-c n I") #'denote-add-links)
-    (define-key map (kbd "C-c n b") #'denote-backlinks)
     (define-key map (kbd "C-c n f f") #'denote-find-link)
     (define-key map (kbd "C-c n f b") #'denote-find-backlink)
+    (define-key map (kbd "C-c n q c") #'denote-query-contents-link)
+    (define-key map (kbd "C-c n q f") #'denote-query-filenames-link)
     (define-key map (kbd "C-c n r") #'denote-rename-file)
-    (define-key map (kbd "C-c n R") #'denote-rename-file-using-front-matter))
+    (define-key map (kbd "C-c n R") #'denote-rename-file-using-front-matter)
+    (define-key map (kbd "C-c n d") #'denote-rename-file-date))
 
   ;; Key bindings specifically for Dired.
   (with-eval-after-load 'dired
@@ -90,7 +88,7 @@ This is a list of strings.")
 (setq org-export-allow-bind-keywords t)
 
 ;;; make denote-link-ol-export support #+export_file_name
-
+;; see also: https://jiewawa.me/2024/03/blogging-with-denote-and-hugo/
 (defun spike-leung/my-denote--get-export-file-name (file)
   "Find #+export_file_name in FILE and return its value.
 Return nil if not found or FILE does not exist."
