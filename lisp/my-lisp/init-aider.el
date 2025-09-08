2a9121d1665bfb997b737ee427c3da375ae84b0a
Spike <l-yanlei@hotmail.com>
2025-09-08 15:50:34 +0800

    :wrench: avoid encrypt authinfo each time start emacs

--- lisp/my-lisp/init-aider.el
+++ lisp/my-lisp/init-aider.el
@@ -3,12 +3,11 @@
 ;;; Code:
 (require 'init-openrouter-models)
 (maybe-require-package 'aider)
-(require 'aider)
 
 (with-eval-after-load 'init-auth
   ;; `with-eval-after-load' 是首次加载时才会执行 body，如果已经加载过了，就不会执行 body
   ;; `eval-after-load' 无关是否首次加载
-  (eval-after-load 'aider
+  (with-eval-after-load 'aider
     (progn
       (setenv "DEEPSEEK_API_KEY" (spike-leung/get-deepseek-api-key))
       (setenv "OPENROUTER_API_KEY" (spike-leung/get-openrouter-api-key))
