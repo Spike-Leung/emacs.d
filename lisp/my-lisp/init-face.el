c3c9d63983fb2ef75134f81a6907d67cdd97813e
Spike <l-yanlei@hotmail.com>
2025-10-17 11:44:54 +0800

    :lipstick: make bold obvious

--- lisp/my-lisp/init-face.el
+++ lisp/my-lisp/init-face.el
@@ -2,10 +2,10 @@
 ;;; Commentary:
 ;;; Code:
 
-(set-face-attribute 'default nil :height 200 :font "Google Sans Code" :font "Iosevka" :font "LXGW WenKai Mono")
+(set-face-attribute 'default nil :height 200 :font "Iosevka" :font "LXGW WenKai Mono")
+(set-face-attribute 'bold nil :font "Iosevka" :weight 'heavy)
 ;;; make background transparent
 ;; (setq frame-background-mode "light")
 ;; (set-face-background 'default "unspecified-bg")
-
 (provide 'init-face)
 ;;; init-face.el ends here
