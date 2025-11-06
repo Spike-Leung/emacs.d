c88072fdb6b580942d623923bd49d178632aceb5
Spike <l-yanlei@hotmail.com>
2025-11-06 15:44:06 +0800

    :wrench: update olivetti setting

--- lisp/my-lisp/init-writing.el
+++ lisp/my-lisp/init-writing.el
@@ -4,6 +4,9 @@
 (maybe-require-package 'olivetti)
 (setq
  olivetti-style 'fancy
- olivetti-body-width 100)
+ olivetti-body-width 100
+ olivetti-margin-width 5)
+(with-eval-after-load 'init-my-keybindings
+  (define-key spike-leung/meta-o-keymap (kbd "o") 'olivetti-mode))
 (provide 'init-writing)
 ;;; init-writing.el ends here
